b8d862ff300eda1734aad55d0c5e9ccdf318f342
John Doe <muzz@bk.ru>
2020-12-08 23:11:52 +0300

    refined output format a bit

--- CMSIS_Parser.py
+++ CMSIS_Parser.py
@@ -15,14 +15,10 @@ register_set = []
 
 for m in re.finditer(r"typedef struct\s+?\{(.*?)\}(\s+|)(\w+)\s*;", s, re.MULTILINE | re.DOTALL):
   type_name = m.group(3)
-  #print (typedef_name + ':')
   g = m.group(1)
 
-  #res = re.sub(r"__\w+?\s+uint\d+_t\s*(\w+);", "\\1", g, 0, re.MULTILINE | re.DOTALL)
   res = re.sub(r".*?((uint\d*?_t)|(_TypeDef))\s+?([A-Za-z\d\[\]]+)\s*;\s*(\/\*\s*(.*?)\s*?\*\/|())", "\\4 `%`\\6\\n", g, 0, re.MULTILINE | re.DOTALL)
   y = res.split('\n')
-
-
 
   register = []
   register.append(type_name)
@@ -43,29 +39,33 @@ for m in re.finditer(r"typedef struct\s+?\{(.*?)\}(\s+|)(\w+)\s*;", s, re.MULTIL
 for x in peripheral:
   for y in register_set:
      if x[1] == y[0]:
-       span = '  /* =================================================================== [' + str(x[0]) + ' init code follows ] '
-       span = span.ljust(186,'=') + '*/'
-       print(span)
+       print(('  /* =================================================================== [' + str(x[0]) + ' initialization block ] ').ljust(186,'=') + '*/')
        for z in y:
          if z != y[0] and z != '':
            rname = re.sub(r'(.*?)\d+', "\\1", x[0], 0, re.DOTALL)
            rgx = '#define\s+(' + rname + '_' + z[0] + '_\S*?)\s+(.*?)((.*)|())'
-           init_str = 'INIT_' + x[0] + '_' + z[0]
-           px =  '  #define ' + init_str + '   ('
-           px = px.ljust(54) + '\\'
-           print(px, end='') 
+           init_str = x[0] + '_' + z[0] + '_INIT'
+           print(('  #define ' + init_str + '   (').ljust(54) + '\\', end='') 
+           def_list = []
            for c in re.finditer(rgx, s, re.MULTILINE):
-             mc = ''
+             txt_comment = ''
              if c.group(4):
-               mc = c.group(4);
-               mc = mc.replace('/*', '').replace('*/', '').replace('!<', '').strip()
-             print('\n    0 * ' + c.group(1).ljust(45), '|  /*', mc.ljust(125), '*/\\', end='')
-           print('\n    0   '.ljust(55) + '\\' + '\n  )')
+               txt_comment = c.group(4);
+               txt_comment = txt_comment.replace('/*', '').replace('*/', '').replace('!<', '').strip()
+
+             def_list.append([c.group(1), txt_comment])
+
+             #print('\n    0 * ' + c.group(1).ljust(45), '|  /*', txt_comment.ljust(125), '*/\\', end='')
+
+           for c in def_list:
+             #print(def_list[-1][1])
+             print('\n    0 * ' + c[0].ljust(45), '|' if def_list[-1][0] != c[0] else " ", ' /*',  c[1].ljust(125), '*/\\', end='')
+           print('\n  )')
+           #print('\n    0   '.ljust(55) + '\\' + '\n  )')
            print('  #if ' + init_str + ' != 0') 
            print('    ' + x[0] + '->' + z[0] + ' = ' + init_str + ';')
            print('  #endif')
-           print('  #undef', init_str)
-           print()
+           print('  #undef', init_str, end = '\n\n\n')
 
            
 
