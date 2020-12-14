d8212ffd3b49b30ed4b2a705097a6aaff433c657
John Doe <muzz@bk.ru>
2020-12-14 22:42:08 +0300

    added function frame and header file generation

--- stm32_init_gen.py
+++ stm32_init_gen.py
@@ -47,24 +47,27 @@ def get_register_bitset(src, register):
 
   return res
 
+
 def make_init_block(src, register_name):
   reg = register_name.split("->")
   reg[0] = re.sub(r"([a-zA-Z]+)(\S*)", "\\1", reg[0], 1)
   r = get_register_bitset(src, reg[0] + '_' + reg[1] + '_')
 
-  def_name = reg[0] + '_' + reg[1] + "_INIT"
+  def_name = f'{reg[0]}_{reg[1]}'
 
-  str_out = ("  #define " + def_name + "  (").ljust(42) + " \\\n"
+  str_out = f'  #define {def_name}  ('.ljust(42) + ' \\\n'
 
   for x in r:
     str_out += "    0 * " + (x[0].ljust(35) + (' ' if x[0] == r[-1][0] else '|') + '  /* ' + ' ' + x[1] + ' ' + x[2].ljust(18) + ' ' + x[3].ljust(60)).ljust(137) + '  */\\\n'
 
-  str_out += "  )\n"
-  str_out += "  #if " + def_name + " != 0\n"
-  str_out += "    " + register_name + " = " + def_name + ";\n"
-  str_out += "  #endif\n"
-  str_out += "  #undef " + def_name + "\n"
+  str_out += f'''  )
+  #if {def_name} != 0
+    {register_name} = {def_name};
+  #endif
+  #undef {def_name}
+  '''
   return str_out
+
 
 def get_registers(s, peripheral):
   for x in re.findall(r'typedef struct\s+{\s+(.*?)}\s*(\S+)\s*;', s, re.MULTILINE | re.DOTALL):
@@ -73,9 +76,11 @@ def get_registers(s, peripheral):
       r_list = res.split('\n')
       res = [x.split('`%`') for x in r_list if len(x) > 1]
       return [[x[0].strip(), x[1].strip('!<').strip()] for x in res if not 'RESERVED' in x[0].upper() and len(x) > 1]
+
      
 peripherals    = lambda s: re.findall('#define\s+?(\w+)\s+\(\((\w+(?:_TypeDef|_GlobalTypeDef))\s+\*.*?(\w+_BASE.*)\)', s, re.MULTILINE)
 register_block = lambda s: [[x[0], get_registers(s, x[1])] for x in peripherals(s)]
+
 
 def init_peripheral(src, peripheral):
   str_out = ''
@@ -84,7 +89,8 @@ def init_peripheral(src, peripheral):
       for y in x[1]:
         str_out += make_init_block(s, x[0] + '->' + y[0]) + '\n'
       str_out += '\n'
-      return str_out
+      return str_out.strip('\n').strip()
+
 
 def init_all(src):
   str_out = ''
@@ -92,7 +98,14 @@ def init_all(src):
     for y in x[1]:
       str_out += make_init_block(s, x[0] + '->' + y[0]) + '\n'
     str_out += '\n'
-  return str_out
+  return str_out.strip('\n').strip()
+
+
+function_name = '__STATIC_INLINE void ' + sys.argv[2].lower() + '_init(void) {\n'
+module_name = sys.argv[2].upper() + '_INIT'
+
+h_file_hdr = '#ifndef __' + module_name +'_H\n#define __' + module_name + '_H\n\n#ifdef __cplusplus\n  extern "C" {\n#endif /* __cplusplus */\n'
+h_file_ftr = '#ifdef __cplusplus\n  }\n#endif /* __cplusplus */\n#endif /* __' + module_name + '_H  */\n'
 
 if __name__ == "__main__":
 
@@ -103,7 +116,8 @@ if __name__ == "__main__":
   s = ''
   buf = ''
   src_file_name = sys.argv[1]
-  url = 'https://raw.githubusercontent.com/STMicroelectronics/STM32Cube' + src_file_name[5:7].upper() + '/master/Drivers/CMSIS/Device/ST/STM32' + src_file_name[5:7].upper() + 'xx/Include/' + src_file_name
+  mcu_family = src_file_name[5:7].upper()
+  url = f'https://raw.githubusercontent.com/STMicroelectronics/STM32Cube{mcu_family}/master/Drivers/CMSIS/Device/ST/STM32{mcu_family}xx/Include/{src_file_name}'
 
   while True:
     try:
@@ -126,6 +140,12 @@ if __name__ == "__main__":
   if sys.argv[2].upper() == "ALL":
     print(init_all(s))
   elif not '->' in sys.argv[2]:
-    print(init_peripheral(s, sys.argv[2]))
+    print(h_file_hdr)
+    print()
+    print(function_name)
+    print(init_peripheral(s, sys.argv[2]), end='')
+    print('\n}')
+    print()
+    print(h_file_ftr)
   else:
     print(make_init_block(s, sys.argv[2]))
